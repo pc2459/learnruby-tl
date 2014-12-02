994257ac940b5a4958f13d3abd04bc1cf8c07406
Fo <chiapolinn@gmail.com>
2014-12-02 12:23:30 +0800

    Finished methods chapter

---
+++ methods.rb
@@ -0,0 +1,51 @@
+## 1 Greeting method
+
+def greeting(name)
+  puts "Hello " + name
+end
+
+puts greeting("Obi-wan")
+
+## 2 
+
+# 2
+
+# nil
+
+# "Joe"
+
+# "four"
+
+# nil
+
+
+## 3 Multiply method
+
+def multiply(a,b)
+  product = a * b
+end
+
+puts multiply(10,3)
+
+## 4
+
+# Nothing will be put to screen
+
+## 5.1
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+## 5.2
+
+# Yippeee!!!!
+
+## 6
+
+# Only one argument was provided to a method requiring two.
+
+ 
